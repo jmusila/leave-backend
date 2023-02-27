754568cc597ae5c8f6cff7bd08a359a0c66b0a41
jmusila <jonathanmusila6@gmail.com>
2023-02-27 13:24:32 +0300

    calculator: add several leave types

--- app/Enums/LeaveTypes.php
+++ app/Enums/LeaveTypes.php
@@ -1,5 +1,12 @@
 <?php
 
-return [
+namespace App\Enums;
 
-];
+class LeaveTypes
+{
+    public const ANNUAL_LEAVE = "Annual Leave";
+
+    public const STUDY_LEAVE = "Study Leave";
+
+    public const COMPASIONATE_LEAVE = "Compasionate Leave";
+}
