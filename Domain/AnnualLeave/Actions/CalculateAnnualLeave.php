5ee1c0a3e638e03464572ca51fee847beaf72947
jmusila <jonathanmusila6@gmail.com>
2023-02-28 11:44:50 +0300

    calculator: add type hinting

--- Domain/AnnualLeave/Actions/CalculateAnnualLeave.php
+++ Domain/AnnualLeave/Actions/CalculateAnnualLeave.php
@@ -8,27 +8,27 @@ use Illuminate\Support\Carbon;
 
 class CalculateAnnualLeave extends BaseAction
 {
-    public function totalWorkingMonths()
+    public function totalWorkingMonths(): int
     {
         return Carbon::parse($this->jobStartDate())->diffInMonths(Carbon::now());
     }
 
-    public function jobStartDate()
+    public function jobStartDate(): string
     {
         return data_get($this->data, 'job_start_date', null);
     }
 
-    public function totalLeaveDaysBeforeDeduction()
+    public function totalLeaveDaysBeforeDeduction(): int
     {
         return multiply($this->totalWorkingMonths(), LeaveTypes::LEAVE_DAYS_PER_MONTH);
     }
 
-    public function totalLeaveDaysAfterDeduction()
+    public function totalLeaveDaysAfterDeduction(): int
     {
         return difference($this->totalLeaveDaysBeforeDeduction(), $this->annualDaysUsed());
     }
 
-    public function annualDaysUsed()
+    public function annualDaysUsed(): int
     {
         return data_get($this->data, 'days_taken_before', null);
     }
