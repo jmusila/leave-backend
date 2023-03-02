bdce7882f8917c3779fd81bd5333ed1c9161f84c
jmusila <jonathanmusila6@gmail.com>
2023-03-02 15:00:57 +0300

    user: added user dto

--- Domain/User/Actions/CreateUser.php
+++ Domain/User/Actions/CreateUser.php
@@ -4,20 +4,14 @@ namespace Domain\User\Actions;
 
 use App\Models\User;
 use Domain\BaseAction;
+use Domain\User\DataTransferObjects\UserData;
 use Illuminate\Support\Facades\Hash;
 
 class CreateUser extends BaseAction
 {
     public function execute(): User
     {
-        $user = User::create([
-            'first_name' => data_get($this->data, 'first_name'),
-            'middle_name' => data_get($this->data, 'middle_name'),
-            'last_name' => data_get($this->data, 'last_name'),
-            'email' => data_get($this->data, 'email'),
-            'phone_number' => data_get($this->data, 'phone_number'),
-            'password' => Hash::make(data_get($this->data, 'password')),
-        ]);
+        $user = User::create(UserData::fromArray($this->data)->toArray());
 
         return $user;
     }
--- Domain/User/DataTransferObjects/UserData.php
+++ Domain/User/DataTransferObjects/UserData.php
@@ -2,7 +2,35 @@
 
 namespace Domain\User\DataTransferObjects;
 
-class UserData
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Facades\Hash;
+
+class UserData implements Arrayable
 {
-    
+    public function __construct(
+        public string $first_name,
+        public string $middle_name,
+        public string $last_name,
+        public string $email,
+        public string $password,
+        public string $phone_number
+    ) {
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            first_name: data_get($data, 'first_name'),
+            middle_name: data_get($data, 'middle_name'),
+            last_name: data_get($data, 'last_name'),
+            email: data_get($data, 'email'),
+            phone_number: data_get($data, 'phone_number'),
+            password: Hash::make(data_get($data, 'password')),
+        );
+    }
+
+    public function toArray(): array
+    {
+        return (array) $this;
+    }
 }
