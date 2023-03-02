f3f38f022495dd8c56ff72f35e9ff62b0c3069e8
jmusila <jonathanmusila6@gmail.com>
2023-03-02 13:02:32 +0300

    user: added validations on request

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -24,7 +24,13 @@ class CreateUserRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'first_name' => ['required', 'string'],
+            'middle_name' => ['filled', 'string'],
+            'last_name' => ['required', 'string'],
+            'phone_number' => ['filled', 'string', 'unique:users'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'confirmed', 'min:8'],
+            'password_confirmation' => ['string', 'min:8'],
         ];
     }
 }
