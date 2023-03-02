6616faa2c7255ebc6b932e1931933c2728ca76fa
jmusila <jonathanmusila6@gmail.com>
2023-03-02 12:56:45 +0300

    user: add create user functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Resources\UserResource;
+use Domain\User\Actions\CreateUser;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function register()
+    public function register(CreateUserRequest $request)
     {
-        
+        $user = (new CreateUser())->fromData($request->validated())->execute();
+
+        return new UserResource($user);
     }
 }
