7f3ee150aab235f088b46ce938bdadf909b93695
jmusila <jonathanmusila6@gmail.com>
2023-03-02 12:49:24 +0300

    user: add resource details

--- app/Http/Resources/CreateUserResource.php
+++ app/Http/Resources/CreateUserResource.php
@@ -14,6 +14,12 @@ class CreateUserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'middle_name' => $this->middle_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+        ];
     }
 }
