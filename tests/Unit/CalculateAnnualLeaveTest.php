7ff47df2fec6ac2ed88e895558d9939add124383
jmusila <jonathanmusila6@gmail.com>
2023-03-28 07:34:40 +0300

    tests: test leave days calculator

--- tests/Unit/CalculateAnnualLeaveTest.php
+++ tests/Unit/CalculateAnnualLeaveTest.php
@@ -15,6 +15,16 @@ it('should return 422 if job start date is missing', function () {
     $response = $this->getJson('api/annual-leave?' . http_build_query(['days_taken_before' => 10]));
 
     $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+    $response->assertJson(['message' => 'The job start date field is required.']);
+});
+
+it('should return 422 if days taken before is missing', function () {
+    $response = $this->getJson('api/annual-leave?' . http_build_query(['job_start_date' => "2021/11/16"]));
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+    $response->assertJson(['message' => 'The days taken before field is required.']);
 });
 
 it('should calculate leave balance successfully', function () use ($data) {
