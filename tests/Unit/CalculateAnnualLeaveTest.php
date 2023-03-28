34d1ed8fe5684e3eb1d93a1a76d43cb1eeda2b9b
jmusila <jonathanmusila6@gmail.com>
2023-03-28 07:25:17 +0300

    tests: test leave days calculator

--- tests/Unit/CalculateAnnualLeaveTest.php
+++ tests/Unit/CalculateAnnualLeaveTest.php
@@ -1,9 +1,24 @@
 <?php
 
-namespace Tests\Unit;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
 
-use Tests\TestCase;
+uses(Tests\TestCase::class);
 
-it('can calculate annual leave and return results', function () {
-    
+$data = [
+    "job_start_date" =>  "2021/11/16",
+    "days_taken_before" => 10
+];
+
+
+it('should return 422 if job start date is missing', function () {
+    $response = $this->getJson('api/annual-leave?' . http_build_query(['days_taken_before' => 10]));
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+});
+
+it('should calculate leave balance successfully', function () use ($data) {
+    $response = $this->getJson('api/annual-leave?' . http_build_query($data));
+
+    $response->assertStatus(Response::HTTP_OK);
 });
