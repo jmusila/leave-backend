dd2ba3f26eaaf720fe3525354c8fe71911e4a4af
jmusila <jonathanmusila6@gmail.com>
2023-04-05 08:23:25 +0300

    users: add user observer

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\Auth\NewUserCreatedNotification;
+use Illuminate\Auth\Passwords\PasswordBroker;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,4 +44,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendVerificationNotification($user)
+    {
+        $token = app(PasswordBroker::class)->createToken($user);
+        $user->notify(new NewUserCreatedNotification($token));
+    }
 }
