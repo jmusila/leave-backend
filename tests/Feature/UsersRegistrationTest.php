a5b4e3f7d9f73ec3b260a06c106205ef761b3506
jmusila <jonathanmusila6@gmail.com>
2023-04-27 08:54:12 +0300

    tests: test create user

--- tests/Feature/UsersRegistrationTest.php
+++ tests/Feature/UsersRegistrationTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use App\Notifications\Auth\VerifyEmail;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Notification;
@@ -8,9 +9,9 @@ use Illuminate\Testing\Fluent\AssertableJson;
 uses(Illuminate\Foundation\Testing\WithFaker::class);
 uses(Illuminate\Foundation\Testing\DatabaseTransactions::class);
 
-
 it('should create a new user successfully', function () {
     Notification::fake();
+
     $userData = [
         "email" =>  $this->faker->unique()->safeEmail(),
         "password" => "password",
@@ -20,11 +21,14 @@ it('should create a new user successfully', function () {
         "last_name" => "Doe",
         "phone_number" => $this->faker->unique()->e164PhoneNumber()
     ];
+
+    $user = User::factory()->create();
+
     $response = $this->postJson('api/create-user', $userData);
 
     $response->assertStatus(Response::HTTP_CREATED);
 
-    $response->assertJson(fn (AssertableJson $json) => $json->where("data.email", $userData["email"]));
+    $response->assertJson(fn (AssertableJson $json) => $json->where("data.email", $userData['email']));
 
-    Notification::assertSentToTimes(1, VerifyEmail::class);
+    Notification::assertSentToTimes($user, VerifyEmail::class);
 });
